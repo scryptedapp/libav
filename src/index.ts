6abfa3cca3efc6411bc7093f47638e663c6bb93c
Koushik Dutta <koushd@gmail.com>
2024-08-24 22:18:44 -0700

    update

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+let addon: any;
+try {
+    addon = require('../build/Release/addon');
+}
+catch (e) {
+}
+
+export function isAvailable() {
+    return !!addon;
+}
+
+export interface AVFrame {
+    close(): void;
+    width: number;
+    height: number;
+    format: string;
+    toBuffer(): Buffer;
+    toJpeg(): Buffer;
+}
+
+export interface AVFilter {
+    destroy(): void;
+    setCrop(x: string, y: string, width: string, height: string): void;
+    filter(frame: AVFrame): AVFrame;
+}
+
+export interface AVFormatContext {
+    open(input: string, codec: string): void;
+    readFrame(): Promise<AVFrame>;
+    createFilter(width: number, height: number, filter: string, useHardware: boolean): AVFilter;
+    close(): void;
+}
+
+export function setAVLogCallback(callback: (msg: string) => void) {
+    addon.set_console_callback(callback);
+}
+
+export function createAVFormatContext(): AVFormatContext {
+    return new addon.AVFormatContext();
+}
