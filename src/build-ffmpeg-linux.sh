773e76a8abe6d9bbc078305afafcc1e80a279831
Koushik Dutta <koushd@gmail.com>
2024-09-04 12:20:04 -0700

    nvidia?

--- src/build-ffmpeg-linux.sh
+++ src/build-ffmpeg-linux.sh
@@ -2,6 +2,15 @@ DEBIAN_FRONTEND=noninteractive
 cd $(dirname $0)/../FFmpeg
 sudo apt -y update
 sudo apt -y install libva-dev libdrm-dev yasm
-./configure --enable-vaapi
+
+ARCH=$(arch)
+if [ "$ARCH" = "x86_64" ]
+then
+    export PATH=/usr/local/cuda-12.4/bin:$PATH
+    ./configure --enable-nonfree --enable-cuda-nvcc --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+else
+    ./configure --enable-vaapi
+fi
+
 make -j32
 exit $?
