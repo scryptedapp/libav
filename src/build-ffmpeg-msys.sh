6a261b974353a1e7cf3b67c1db04670c37422fb9
Koushik Dutta <koushd@gmail.com>
2024-09-07 13:42:28 -0700

    win env fixes

--- src/build-ffmpeg-msys.sh
+++ src/build-ffmpeg-msys.sh
@@ -1,8 +1,12 @@
+cd $(dirname $0)/../nv-codec-headers
+make install PREFIX=../_nvinstall
+
 cd $(dirname $0)/../FFmpeg
 pacman --noconfirm -S yasm pkg-config clang diffutils make
-export PKG_CONFIG_PATH=/c/Users/koush/OneDrive/Desktop/work/_vplinstall/lib/pkgconfig:/c/Users/koush/OneDrive/Desktop/work/_nvinstall/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$PWD/../_vplinstall/lib/pkgconfig:$PWD/../_nvinstall/lib/pkgconfig:$PKG_CONFIG_PATH
 
 # the --enable-nvdec is necessary for build sanity checking, the build will configure
 # successfully if nvidia stuff is missing, even though it is required for --enable-cuda-llvm
-./configure --enable-libvpl --enable-cuda-llvm --enable-nvdec --toolchain=msvc && make -j32
+./configure --enable-libvpl --enable-cuda-llvm --enable-nvdec --toolchain=msvc
+make -j32
 exit $?
