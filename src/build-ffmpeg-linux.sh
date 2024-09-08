9f23a2bd671cf2fd029778c49cf83782c352659d
Koushik Dutta <koushd@gmail.com>
2024-09-08 10:05:01 -0700

    vpl

--- src/build-ffmpeg-linux.sh
+++ src/build-ffmpeg-linux.sh
@@ -6,6 +6,14 @@ sudo apt -y install libva-dev libdrm-dev yasm
 ARCH=$(arch)
 if [ "$ARCH" = "x86_64" ]
 then
+    pushd ../libvpl
+    export VPL_INSTALL_DIR=`pwd`/../_vplinstall
+    cmake -B _build -DCMAKE_INSTALL_PREFIX=$VPL_INSTALL_DIR -DBUILD_SHARED_LIBS=OFF
+    cmake --build _build
+    cmake --install _build
+    export PKG_CONFIG_PATH=$PWD/../_vplinstall/lib/pkgconfig:$PKG_CONFIG_PATH
+    popd
+
     pushd ../nv-codec-headers
     make install PREFIX=../_nvinstall
     export PKG_CONFIG_PATH=$PWD/../_nvinstall/lib/pkgconfig:$PKG_CONFIG_PATH
