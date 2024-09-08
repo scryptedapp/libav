20e6dbad872701822c98378a5c8970922fea324a
Koushik Dutta <koushd@gmail.com>
2024-09-08 09:51:46 -0700

    sanity checks

--- src/build-ffmpeg-linux.sh
+++ src/build-ffmpeg-linux.sh
@@ -7,13 +7,19 @@ ARCH=$(arch)
 if [ "$ARCH" = "x86_64" ]
 then
     pushd ../nv-codec-headers
-    sudo make install
+    make install PREFIX=../_nvinstall
+    export PKG_CONFIG_PATH=$PWD/../_nvinstall/lib/pkgconfig:$PKG_CONFIG_PATH
     popd
     echo "Building with NVIDIA GPU and Vulkan support"
     export PATH=/usr/local/cuda-12.4/bin:$PATH
-    ./configure --enable-vaapi --enable-libglslang --enable-cuda-llvm --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+    ./configure --enable-vaapi --enable-libglslang --enable-cuda-llvm --enable-nvdec --extra-cflags=-I/usr/local/cuda/i>
 else
     ./configure --enable-vaapi
+fi
+
+if [ "$?" != "0" ]
+then
+  exit 1
 fi
 
 make -j32
