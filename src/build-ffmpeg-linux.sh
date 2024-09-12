4b92e11374385b96197a40135d7d8d9e8d6e0c29
Koushik Dutta <koushd@gmail.com>
2024-09-11 21:44:19 -0700

    ocl-icd-opencl-dev

--- src/build-ffmpeg-linux.sh
+++ src/build-ffmpeg-linux.sh
@@ -1,7 +1,7 @@
 DEBIAN_FRONTEND=noninteractive
 cd $(dirname $0)/../FFmpeg
 sudo apt -y update
-sudo apt -y install libva-dev libdrm-dev yasm cmake
+sudo apt -y install libva-dev libdrm-dev yasm cmake ocl-icd-opencl-dev
 
 ARCH=$(arch)
 if [ "$ARCH" = "x86_64" ]
