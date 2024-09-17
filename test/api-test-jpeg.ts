07d59381f44f3f462e0942e812773e2e1a96cb54
Koushik Dutta <koushd@gmail.com>
2024-09-16 21:56:17 -0700

    wip

--- test/api-test-jpeg.ts
+++ test/api-test-jpeg.ts
@@ -1,4 +1,5 @@
-import { AVCodecContext, createAVFormatContext, setAVLogLevel } from '../src';
+import fs from 'fs';
+import { setAVLogLevel, createAVFormatContext, AVCodecContext } from '../src';
 
 async function main() {
     setAVLogLevel('verbose');
@@ -6,7 +7,6 @@ async function main() {
 
     ctx.open("rtsp://scrypted-nvr:50757/68c1f365ed3e15b4");
     const decoder = ctx.createDecoder('videotoolbox');
-    let encoder: AVCodecContext|undefined;
     console.log('opened', ctx.metadata, decoder.hardwareDevice, decoder.pixelFormat, decoder.hardwarePixelFormat);
 
     while (true) {
@@ -27,19 +27,25 @@ async function main() {
         if (!frame)
             continue;
 
-        // reusing the jpeg encoder seems to cause several quality loss after the first frame
-        if (!encoder) {
-            encoder = frame.createEncoder({
-                encoder: 'h264_videotoolbox',
-                bitrate: 2000000,
-                timeBaseNum: ctx.timeBaseNum,
-                timeBaseDen: ctx.timeBaseDen,
-            });
-        }
+        using filter = ctx.createFilter(frame.width, frame.height, 'hwdownload,format=nv12,scale,format=yuvj420p', decoder);
+        using softwareFrame = filter.filter(frame);
+
+        // reusing a jpeg encoder seems to cause several quality loss after the first frame
+        using encoder = softwareFrame.createEncoder({
+            encoder: 'mjpeg',
+            bitrate: 2000000,
+            timeBaseNum: ctx.timeBaseNum,
+            timeBaseDen: ctx.timeBaseDen,
+            opts: {
+                quality: 1,
+                qmin: 1,
+                qmax: 1,
+            }
+        });
 
         const sent = await encoder.sendFrame(frame);
         if (!sent) {
-            console.error('sendFrame failed, frame will be dropped?');
+            console.error('sendFrame failed');
             continue;
         }
 
