f7abbb3d54fbdb206c77ffe4765530e0864152d1
Koushik Dutta <koushd@gmail.com>
2024-09-17 21:44:22 -0700

    typo

--- test/read-receive-send-receive.ts
+++ test/read-receive-send-receive.ts
@@ -27,7 +27,7 @@ async function main() {
         if (!packet)
             continue;
 
-        if (packet.streamindex !== video.index)
+        if (packet.streamIndex !== video.index)
             continue;
 
         try {
