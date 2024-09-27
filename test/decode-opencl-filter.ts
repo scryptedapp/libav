d6dece5e0b88dce6e26f2acf3380d5ce294c0e43
Koushik Dutta <koushd@gmail.com>
2024-09-27 15:42:32 -0700

    opencl blur

--- test/decode-opencl-filter.ts
+++ test/decode-opencl-filter.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { AVFilter, createAVFilter, createAVFormatContext, setAVLogLevel } from '../src';
 
 async function main() {
@@ -17,7 +18,7 @@ async function main() {
 
         if (!openclFilter) {
             openclFilter = createAVFilter({
-                filter: 'hwdownload,format=nv12,hwupload,program_opencl=kernel=set_black',
+                filter: 'hwdownload,format=nv12,hwupload,program_opencl=kernel=blur,hwdownload,format=nv12,scale,format=yuvj420p',
                 hardwareDevice: 'opencl',
                 frames: [
                     {
@@ -28,18 +29,38 @@ async function main() {
             });
 
             openclFilter.sendCommand("program_opencl", "source", `
-                __kernel void set_black(__write_only image2d_t output_image, unsigned int index,
+                __kernel void blur(__write_only image2d_t output_image, unsigned int index,
                         __read_only image2d_t input_image) {
-                int x = get_global_id(0);
-                int y = get_global_id(1);
+                    int x = get_global_id(0);
+                    int y = get_global_id(1);
+                    int width = get_image_width(input_image);
+                    int height = get_image_height(input_image);
 
-                int2 coords = (int2)(x, y);
+                    int2 coords = (int2)(x, y);
 
-                // Set pixel to black with full opacity (RGBA = 0, 0, 0, 255)
-                uint4 black_pixel = (uint4)(0, 0, 0, 255);
+                    if (x == 0 || y == 0 || x == width - 1 || y == height - 1) {
+                        uint4 src = read_imageui(input_image, coords);
+                        write_imageui(output_image, coords, src);
+                        return;
+                    }
 
-                // Write the black pixel to the output image
-                write_imageui(output_image, coords, black_pixel);
+                    uint4 tl = read_imageui(input_image, coords + (int2)(-1, -1));
+                    uint4 t = read_imageui(input_image, coords + (int2)(0, -1));
+                    uint4 tr = read_imageui(input_image, coords + (int2)(1, -1));
+                    uint4 l = read_imageui(input_image, coords + (int2)(-1, 0));
+                    uint4 c = read_imageui(input_image, coords);
+                    uint4 r = read_imageui(input_image, coords + (int2)(1, 0));
+                    uint4 bl = read_imageui(input_image, coords + (int2)(-1, 1));
+                    uint4 b = read_imageui(input_image, coords + (int2)(0, 1));
+                    uint4 br = read_imageui(input_image, coords + (int2)(1, 1));
+
+                    uint4 sumCorners = tl + tr + bl + br;
+                    uint4 sixteenthCorners = sumCorners / 16;
+                    uint4 sumSides = t + l + r + b;
+                    uint4 eighthSides = sumSides / 8;
+                    uint4 result = c / 4 + sixteenthCorners + eighthSides;
+
+                    write_imageui(output_image, coords, result);
                 }
             `)
         }
@@ -47,6 +68,10 @@ async function main() {
         openclFilter.addFrame(frame);
         using clframe = openclFilter.getFrame();
         console.log(clframe.pixelFormat);
+
+        // const jpeg = clframe.toJpeg(1);
+        // // save it
+        // fs.writeFileSync('/tmp/output.jpg', jpeg);
     }
 }
 
