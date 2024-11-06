80d9186fdcc9f1ffe5a83b2d871e5aff1434ea05
Koushik Dutta <koushd@gmail.com>
2024-11-06 15:12:30 -0800

    windows vulkan support

--- src/build-ffmpeg-msys.sh
+++ src/build-ffmpeg-msys.sh
@@ -22,10 +22,14 @@ fi
 
 # yasm can be dropped
 pacman --noconfirm -S yasm nasm pkg-config clang diffutils make
-export PKG_CONFIG_PATH=$PWD/../_vplinstall/lib/pkgconfig:$PWD/../_nvinstall/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$PWD/../_vplinstall/lib/pkgconfig:$PWD/../_nvinstall/lib/pkgconfig:$PWD/../src/pkgconfig:$PKG_CONFIG_PATH
 
 # the --enable-nvdec is necessary for build sanity checking, the build will configure
 # successfully if nvidia stuff is missing, even though it is required for --enable-cuda-llvm
 # no open cl yet, need to do this https://github.com/m-ab-s/media-autobuild_suite/issues/1301#issuecomment-627800902
-./configure --enable-libvpl --enable-cuda-llvm --enable-nvdec --toolchain=msvc && make -j32
+
+# this version is the last that has HLSL and OGLCompiler which are still required by ffmpeg for now
+# https://sdk.lunarg.com/sdk/download/1.3.268.0/windows/VulkanSDK-1.3.268.0-Installer.exe
+
+./configure --enable-libvpl --enable-libglslang --enable-cuda-llvm --enable-nvdec --toolchain=msvc && make -j32
 exit $?
