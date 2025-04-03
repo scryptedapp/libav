751962332d8729e8bea9a6484c951d28be24b906
Koushik Dutta <koushd@gmail.com>
2025-04-03 15:05:54 -0700

    fix memory leak and simplify allocs

--- src/worker/read-frame-worker.cpp
+++ src/worker/read-frame-worker.cpp
@@ -8,84 +8,99 @@ ReadFrameWorker::ReadFrameWorker(napi_env env, napi_deferred deferred, AVFormatC
 {
 }
 
-void ReadFrameWorker::Execute() {
+void ReadFrameWorker::Execute()
+{
     frameResult = nullptr;
     packetResult = nullptr;
 
     AVFormatContext *fmt_ctx_ = formatContextObject->fmt_ctx_;
-    if (!fmt_ctx_) {
+    if (!fmt_ctx_)
+    {
         SetError("Format context is null");
         return;
     }
 
     AVCodecContext *codecContext = nullptr;
-    AVFrame *frame;
-    if (codecContextObject) {
+    if (codecContextObject)
+    {
         codecContext = codecContextObject->codecContext;
-        if (!codecContext) {
+        if (!codecContext)
+        {
             SetError("Codec context is null");
             return;
         }
-        frame = av_frame_alloc();
-    }
-
-    AVPacket *packet = av_packet_alloc();
-
-    if (!packet) {
-        SetError("Failed to allocate packet");
-        return;
     }
 
     int ret;
-    while (true) {
-        if (codecContext) {
+    while (true)
+    {
+        if (codecContext)
+        {
+            AVFrame *frame = av_frame_alloc();
+            if (!frame)
+            {
+                SetError("Failed to allocate frame");
+                return;
+            }
+
             // attempt to read frames first, EAGAIN will be returned if data needs to be
             // sent to decoder.
             ret = avcodec_receive_frame(codecContext, frame);
-            if (!ret) {
-                av_packet_free(&packet);
+            if (!ret)
+            {
                 frameResult = frame;
                 return;
             }
-            else if (ret != AVERROR(EAGAIN)) {
-                av_packet_free(&packet);
+
+            av_frame_free(&frame);
+            if (ret != AVERROR(EAGAIN))
+            {
                 SetError(AVErrorString(ret));
                 return;
             }
         }
 
-        while (true) {
-            ret = av_read_frame(fmt_ctx_, packet);
-            if (ret == AVERROR(EAGAIN)) {
-                // try reading again later
-                av_packet_free(&packet);
-                return;
-            }
-            else if (ret) {
-                av_packet_free(&packet);
-                SetError(AVErrorString(ret));
+        while (true)
+        {
+            AVPacket *packet = av_packet_alloc();
+
+            if (!packet)
+            {
+                SetError("Failed to allocate packet");
                 return;
             }
 
-            if (!codecContext) {
+            ret = av_read_frame(fmt_ctx_, packet);
+            if (ret)
+            {
+                av_packet_free(&packet);
+                // EAGAIN can try reading again later
+                if (ret != AVERROR(EAGAIN))
+                    SetError(AVErrorString(ret));
+                return;
+            }
+
+            if (!codecContext)
+            {
                 packetResult = packet;
                 return;
             }
 
-            if (packet->stream_index != streamIndex) {
+            if (packet->stream_index != streamIndex)
+            {
                 // wrong stream, return the packet
                 packetResult = packet;
                 return;
             }
 
             ret = avcodec_send_packet(codecContext, packet);
-            av_packet_unref(packet);
+            av_packet_free(&packet);
 
             // on decoder feed error, try again next packet.
             // could be starting on a non keyframe or data corruption
             // which may be recoverable.
-            if (ret) {
-                av_packet_free(&packet);
+            if (ret)
+            {
                 return;
             }
 
@@ -97,20 +112,25 @@ void ReadFrameWorker::Execute() {
     }
 }
 
-void ReadFrameWorker::OnOK() {
+void ReadFrameWorker::OnOK()
+{
     Napi::Env env = Env();
-    if (packetResult) {
+    if (packetResult)
+    {
         napi_resolve_deferred(Env(), deferred, AVPacketObject::NewInstance(env, packetResult));
     }
-    else if (frameResult) {
+    else if (frameResult)
+    {
         napi_resolve_deferred(Env(), deferred, AVFrameObject::NewInstance(env, frameResult));
     }
-    else {
+    else
+    {
         napi_resolve_deferred(Env(), deferred, env.Undefined());
     }
 }
 
-void ReadFrameWorker::OnError(const Napi::Error &e) {
+void ReadFrameWorker::OnError(const Napi::Error &e)
+{
     napi_value error = e.Value();
     napi_reject_deferred(Env(), deferred, error);
 }
